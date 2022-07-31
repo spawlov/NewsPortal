7d7bca3485113bce795865022d909cd8ce206e77
spawlov <spawlov@gmail.com>
2022-07-31 17:20:31 +0700

    changed censor.py

--- NewsPaper/news/templatetags/censor.py
+++ NewsPaper/news/templatetags/censor.py
@@ -7,13 +7,14 @@ register = template.Library()
 
 @register.filter()
 def censor(in_text):
+    censured_text = in_text
     with open('static/json/bad_words.json', 'r') as censor_file:
         bad_text = json.load(censor_file)
 
-    for word in in_text.split():
+    for word in censured_text.split():
         if word.lower() in bad_text:
-            in_text = in_text.replace(
+            censured_text = censured_text.replace(
                 word,
                 f'{word[0]}{"*" * (len(word) - 1)}'
             )
-    return in_text
+    return censured_text
