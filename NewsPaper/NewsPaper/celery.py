e3c8877425ed8b1e474dbc3dafdd2b41b9a6a7f9
sergey <spawlov@gmail.com>
2022-08-29 19:30:22 +0700

    changed celery.py

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -7,15 +7,20 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# app.conf.beat_schedule = {
+#     # Executes every Monday morning at 8:00
+#     'add-weekly-mailings': {
+#         'task': 'news.tasks.weekly_notify',
+#         'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+#     },
+# }
+
 app.conf.beat_schedule = {
     # Executes every Monday morning at 8:00
     'add-weekly-mailings': {
         'task': 'news.tasks.weekly_notify',
-        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        'schedule': crontab(hour=12, minute=0, day_of_week='monday'),
     },
-}
-
-app.conf.beat_schedule = {
     # Executes every Day at 0:00
     'parsing-for-add-news': {
         'task': 'news.tasks.daily_parsing',
